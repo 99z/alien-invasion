c9846ea5e4dc6f6dee8224b6ca24e0ace4c2a3e9
Nicolas S. Montanaro <nsmon@rbox.co>
2018-05-07 11:53:25 -0400

    clean + comment

--- main.go
+++ main.go
@@ -12,17 +12,6 @@ import (
 	"time"
 )
 
-// QUESTIONS
-// 1. Is numAliens < numCities? Any restriction on these?
-// 2. Is a road always a 2-way connection? Meaning, if NY says it is connected
-// to Boston, is does Boston say it is connected to NY?
-
-// CONCERNS
-// 1. Does city need to be destroyed as soon as move is made?
-// 2. What if 2 aliens begin in same city, but additional alien
-// moves to city prior to iterator handling what happens for that city?
-// We will have 3 aliens in a city in this case
-
 // Invasion represents an active simulation and its state
 type Invasion struct {
 	cities       map[string][]string
@@ -30,6 +19,8 @@ type Invasion struct {
 	uniqueCities []string
 }
 
+// Creates new Invasion, populates cities from a file, populates aliens
+// from program arg, runs simulation, writes map state to a file
 func main() {
 	state := new(Invasion)
 	state.initInvasion()
@@ -46,16 +37,20 @@ func main() {
 
 	state.runSimulation()
 
-	state.printMapState()
+	state.writeMapState()
 }
 
+// initInvasion initializes Invasion attributes to empty data structures
 func (invasion *Invasion) initInvasion() {
 	invasion.cities = make(map[string][]string)
 	invasion.aliens = make(map[string][]int)
 	invasion.uniqueCities = make([]string, 0)
 }
 
-func (invasion *Invasion) printMapState() {
+// writeMapState writes the end state of the invasion simulation to a file
+// in the same format as the input file
+// Assumptions: can't specify result filename
+func (invasion *Invasion) writeMapState() {
 	f, err := os.Create("result")
 	if err != nil {
 		log.Fatal(err)
@@ -72,10 +67,6 @@ func (invasion *Invasion) printMapState() {
 	}
 
 	w.Flush()
-}
-
-func (invasion *Invasion) formatNeighbors(city string) {
-	// asd
 }
 
 // populateCities reads data from a file called "cities"
@@ -96,41 +87,41 @@ func (invasion *Invasion) populateCities() {
 	// Iterate over lines of cities file
 	for scanner.Scan() {
 		// Split city line by spaces
-		c := strings.Split(scanner.Text(), " ")
+		currentCity := strings.Split(scanner.Text(), " ")
 
 		// If this city does not already exist
-		if len(invasion.cities[c[0]]) == 0 {
+		if len(invasion.cities[currentCity[0]]) == 0 {
 			// Create new city with name of current line
-			invasion.cities[c[0]] = make([]string, 0)
+			invasion.cities[currentCity[0]] = make([]string, 0)
 
 			// Add to uniqueCities for use in alien assignments
-			invasion.uniqueCities = append(invasion.uniqueCities, c[0])
+			invasion.uniqueCities = append(invasion.uniqueCities, currentCity[0])
 		}
-
-		// Regex to filter out direction and equals sign
-		// reg, err := regexp.Compile(`^(.*?)=`)
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
 
 		// Add neighbors of current city
-		// TODO Probably good to make sure this isn't the same as current city
-		for _, neighbor := range c[1:] {
-			// filtered := reg.ReplaceAllString(neighbor, "")
+		for _, neighbor := range currentCity[1:] {
+			// Skip neighbor if it is the same as current city
+			if strings.Contains(neighbor, currentCity[0]) {
+				continue
+			}
 
-			invasion.cities[c[0]] = append(invasion.cities[c[0]], neighbor)
+			invasion.cities[currentCity[0]] = append(invasion.cities[currentCity[0]], neighbor)
 		}
 	}
-}
 
-func (invasion *Invasion) populateAliens(numAliens int) {
+	// Throw error and exit if no cities were created
 	if len(invasion.uniqueCities) == 0 || len(invasion.cities) == 0 {
 		log.Fatalf("populateAliens: must populate cities first")
 	}
+}
 
+// populateAliens creates numAliens and randomly places them in a city
+// Assumption: no more than 2 aliens may begin in the same city
+func (invasion *Invasion) populateAliens(numAliens int) {
 	rand.Seed(time.Now().Unix())
 
 	for i := 0; i < numAliens; i++ {
+		// Pick random city
 		city := invasion.uniqueCities[rand.Intn(len(invasion.uniqueCities))]
 
 		// Ensure no cities have more than 2 aliens
@@ -138,32 +129,43 @@ func (invasion *Invasion) populateAliens(numAliens int) {
 			city = invasion.uniqueCities[rand.Intn(len(invasion.uniqueCities))]
 		}
 
+		// Append alien to city
 		invasion.aliens[city] = append(invasion.aliens[city], i)
 	}
 }
 
+// destroyCity deletes a city, specified by a string, from the cities map
+// and alien locations map
+// When destroying a city, visits its own neighbors and removes itself from
+// their list of neighbors
 func (invasion *Invasion) destroyCity(city string) {
-	fmt.Printf("%v has been destroyed by alien %v and alien %v!\n", city, invasion.aliens[city][0], invasion.aliens[city][1])
-
+	// Regex to filter out direction prefix
 	reg, err := regexp.Compile(`^(.*?)=`)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	fmt.Printf("%v has been destroyed by alien %v and alien %v!\n", city, invasion.aliens[city][0], invasion.aliens[city][1])
+
 	// Go to neighbors of deleted and delete itself from their lists
 	for _, neighbor := range invasion.cities[city] {
 		neighbor = reg.ReplaceAllString(neighbor, "")
 		for i, n := range invasion.cities[neighbor] {
+			// Neighbors contain the direction prefix, so check that city
+			// is a substring of the neighbor instead of direct comparison
 			if strings.Contains(n, city) {
 				invasion.cities[neighbor][i] = invasion.cities[neighbor][len(invasion.cities[neighbor])-1]
 				invasion.cities[neighbor] = invasion.cities[neighbor][:len(invasion.cities[neighbor])-1]
 			}
 		}
 	}
+
 	delete(invasion.aliens, city)
 	delete(invasion.cities, city)
 }
 
+// runSimulation runs the invasion simulation until one of two conditions is
+// met: all aliens have died, or 10k turns have passed
 func (invasion *Invasion) runSimulation() {
 	turns := 0
 	for len(invasion.aliens) != 0 && turns < 10000 {
@@ -184,6 +186,9 @@ func (invasion *Invasion) runSimulation() {
 			} else if len(invasion.aliens[city]) == 1 {
 				rand.Seed(time.Now().Unix())
 
+				// Regex to filter out direction prefix so we can properly move
+				// This is because format of cities map is a non-prefixed
+				// cites mapped to an array of direction-prefixed cities
 				reg, err := regexp.Compile(`^(.*?)=`)
 				if err != nil {
 					log.Fatal(err)
